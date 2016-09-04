79ba49dcae10aaead484d46e0db86a77d5112d5c
Ben Watson <benwatson528@gmail.com>
2016-09-04 15:52:51 +0100

    Getting the Kafka Streams Drools pipeline working within the integration test

--- src/test/java/uk/co/hadoopathome/kafkastreams/integration/StreamingDroolsIntegrationTest.java
+++ src/test/java/uk/co/hadoopathome/kafkastreams/integration/StreamingDroolsIntegrationTest.java
@@ -18,13 +18,15 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.*;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import uk.co.hadoopathome.kafkastreams.drools.DroolsSessionFactory;
+import uk.co.hadoopathome.kafkastreams.drools.Message;
 import uk.co.hadoopathome.kafkastreams.integration.utils.EmbeddedSingleNodeKafkaCluster;
 import uk.co.hadoopathome.kafkastreams.integration.utils.IntegrationTestUtils;
 
@@ -41,94 +43,57 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class StreamingDroolsIntegrationTest {
 
-    @ClassRule public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
+    @ClassRule
+    public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
 
     private static final String inputTopic = "inputTopic";
     private static final String outputTopic = "outputTopic";
 
-    @BeforeClass public static void startKafkaCluster() throws Exception {
+    @BeforeClass
+    public static void startKafkaCluster() throws Exception {
         CLUSTER.createTopic(inputTopic);
         CLUSTER.createTopic(outputTopic);
     }
 
-    @Test public void shouldCountWords() throws Exception {
-        List<String>
-                inputValues =
-                Arrays.asList("Hello Kafka Streams", "All streams lead to Kafka", "Join Kafka Summit");
-        List<KeyValue<String, Long>>
-                expectedWordCounts =
-                Arrays.asList(new KeyValue<>("hello", 1L), new KeyValue<>("kafka", 1L), new KeyValue<>("streams", 1L),
-                        new KeyValue<>("all", 1L), new KeyValue<>("streams", 2L), new KeyValue<>("lead", 1L),
-                        new KeyValue<>("to", 1L), new KeyValue<>("kafka", 2L), new KeyValue<>("join", 1L),
-                        new KeyValue<>("kafka", 3L), new KeyValue<>("summit", 1L));
+    @Test
+    public void shouldCountWords() throws Exception {
+        List<String> inputValues = Arrays.asList("Hello", "Canal", "Camel");
+        List<String> expectedOutput = Arrays.asList("0Hello", "Canal", "0Camel");
 
-        //
-        // Step 1: Configure and start the processor topology.
-        //
-        // This code is identical to what's shown in WordCountLambdaExample, with the exception of
-        // the call to `purgeLocalStreamsState()`, which we need only for cleaning up previous runs of
-        // this integration test.
-        //
-        final Serde<String> stringSerde = Serdes.String();
-        final Serde<Long> longSerde = Serdes.Long();
+        Properties streamsConfiguration = createStreamConfig();
 
-        Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-integration-test");
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
-        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, CLUSTER.zookeeperConnect());
-        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        // Explicitly place the state directory under /tmp so that we can remove it via
-        // `purgeLocalStreamsState` below.  Once Streams is updated to expose the effective
-        // StreamsConfig configuration (so we can retrieve whatever state directory Streams came up
-        // with automatically) we don't need to set this anymore and can update `purgeLocalStreamsState`
-        // accordingly.
-        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams");
-
-        // Remove any state from previous test runs
         IntegrationTestUtils.purgeLocalStreamsState(streamsConfiguration);
 
         KStreamBuilder builder = new KStreamBuilder();
 
-        KStream<String, String> textLines = builder.stream(inputTopic);
+        KStream<byte[], String> inputData = builder.stream(inputTopic);
+        KStream<byte[], String> outputData = inputData.mapValues(this::applyRule);
 
-        KStream<String, Long>
-                wordCounts =
-                textLines.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                        .map((key, word) -> new KeyValue<>(word, word)).countByKey(stringSerde, "Counts").toStream();
-
-        wordCounts.to(stringSerde, longSerde, outputTopic);
+        outputData.to(outputTopic);
 
         KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
         streams.start();
 
-        //
-        // Step 2: Produce some input data to the input topic.
-        //
-        Properties producerConfig = new Properties();
-        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
-        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
-        producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
-        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        Properties producerConfig = createProducerConfig();
         IntegrationTestUtils.produceValuesSynchronously(inputTopic, inputValues, producerConfig);
 
-        //
-        // Step 3: Verify the application's output data.
-        //
-        Properties consumerConfig = new Properties();
-        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
-        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "wordcount-lambda-integration-test-standard-consumer");
-        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
-        List<KeyValue<String, Long>>
-                actualWordCounts =
-                IntegrationTestUtils
-                        .waitUntilMinKeyValueRecordsReceived(consumerConfig, outputTopic, expectedWordCounts.size());
+        Properties consumerConfig = createConsumerConfig();
+        List<String> actualOutput = IntegrationTestUtils
+                .waitUntilMinValuesRecordsReceived(consumerConfig, outputTopic, expectedOutput.size());
         streams.close();
-        assertThat(actualWordCounts).containsExactlyElementsOf(expectedWordCounts);
+        assertThat(actualOutput).containsExactlyElementsOf(expectedOutput);
+    }
+
+    private Properties createStreamConfig() {
+        Properties streamsConfiguration = new Properties();
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-integration-test");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, CLUSTER.zookeeperConnect());
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/kafka-streams");
+        return streamsConfiguration;
     }
 
     private Properties createProducerConfig() {
@@ -136,7 +101,7 @@ public class StreamingDroolsIntegrationTest {
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
         producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
         producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
-        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class);
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return producerConfig;
     }
@@ -146,8 +111,16 @@ public class StreamingDroolsIntegrationTest {
         consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
         consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "wordcount-lambda-integration-test-standard-consumer");
         consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return consumerConfig;
+    }
+
+    private String applyRule(String value) {
+        KieSession kieSession = DroolsSessionFactory.createDroolsSession("IfContainsEPrepend0KS");
+        Message message = new Message(value);
+        kieSession.insert(message);
+        kieSession.fireAllRules();
+        return message.getContent();
     }
 }
